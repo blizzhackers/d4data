47301dafe342552ffd30285d84096f69c739a13a
Nishimura-Katsuo <38635886+Nishimura-Katsuo@users.noreply.github.com>
2023-04-17 10:20:37 -0600

    Added checksum calculator.

---
+++ checksum/checksum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+uint32_t checksum(std::string str) {
+  uint32_t hash = 0;
+  for (size_t i = 0; i < str.length(); i++) {
+    hash = hash * 33 + str[i];
+  }
+  return hash;
+}
+
+int main(int argc, char *argv[]) {
+  std::string tmp = "";
+
+  for (int c = 1; c < argc; c++) {
+    std::cout << argv[c] << ": " << std::hex << checksum(argv[c]) << std::endl;
+  }
+
+  return 0;
+}
