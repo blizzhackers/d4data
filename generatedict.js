2b6f039f3dbf909bebcee6a2dd61bccf6604db7a
Nishimura-Katsuo <38635886+Nishimura-Katsuo@users.noreply.github.com>
2023-05-06 16:12:08 -0600

    Added prefix/dict lists.

---
+++ generatedict.js
@@ -0,0 +1,106 @@
+const fs = require('fs');
+
+const prefix = {
+  'm_': 'm_',
+};
+
+const dict = {};
+
+let names = {};
+let newnames = {};
+
+fs.readFileSync('names.txt').toString().split(/\s+/gi).forEach(name => {
+  names[name] = name;
+});
+
+Object.keys(names).forEach(name => {
+  let matches = name.split(/^((?:fl|u|n|s|sz|pt|t|m|a|ar|b|bc|dw|e|f|gb|h|i|is|id|l|p|pn|q|v|vw|wd|wp|wr|ws|wv)(?=[0-9A-Z_]))/g).filter(Boolean);
+
+  matches.forEach(subname => {
+    if (/^[^a-zA-Z]+$/.test(subname)) {
+      return;
+    }
+
+    if (/^(fl|u|n|s|sz|pt|t|m|a|ar|b|bc|dw|e|f|gb|h|i|is|id|l|p|pn|q|v|vw|wd|wp|wr|ws|wv)$/g.test(subname)) {
+      prefix[subname.toLowerCase()] = names[name];
+    } else {
+      newnames[subname] = names[name];
+    }
+  });
+});
+
+names = newnames;
+newnames = {};
+
+Object.keys(names).forEach(name => {
+  let matches = name.split(/(3x3|4x4|BCVEC2I|IDs(?![a-z])|By(?![a-z])|To(?![a-z])|In(?![a-z])|Is(?![a-z])|On(?![a-z])|No(?![a-z])|(?<![0-9])2D|(?<![0-9])3D|(?<![0-9])4D)/g).filter(Boolean);
+
+  matches.forEach(subname => {
+    if (/^([^a-zA-Z]+)$/.test(subname)) {
+      return;
+    }
+
+    if (/^(3x3|4x4|BCVEC2I|IDs(?![a-z])|By(?![a-z])|To(?![a-z])|In(?![a-z])|Is(?![a-z])|On(?![a-z])|No(?![a-z])|(?<![0-9])2D|(?<![0-9])3D|(?<![0-9])4D)$/g.test(subname) || /^[a-z]{3,}$/g.test(subname)) {
+      dict[subname.toLowerCase()] = names[name];
+    } else {
+      newnames[subname] = names[name];
+    }
+  });
+});
+
+names = newnames;
+newnames = {};
+
+Object.keys(names).forEach(name => {
+  let matches = name.split(/([A-Z][a-z]{2,})/g).filter(Boolean);
+
+  matches.forEach(subname => {
+    if (/^[^a-zA-Z]+$/.test(subname)) {
+      return;
+    }
+
+    if (/^([A-Z][a-z]+)$/g.test(subname) || /^[a-z]{3,}$/g.test(subname)) {
+      dict[subname.toLowerCase()] = names[name];
+    } else {
+      newnames[subname] = names[name];
+    }
+  });
+});
+
+names = newnames;
+newnames = {};
+
+Object.keys(names).forEach(name => {
+  let matches = name.split(/([A-Z]{2,})/g).filter(Boolean);
+
+  matches.forEach(subname => {
+    if (/^[^a-zA-Z]+$/.test(subname)) {
+      return;
+    }
+
+    if (/^([A-Z]{2,})$/g.test(subname) || /^[a-z]{3,}$/g.test(subname)) {
+      dict[subname.toLowerCase()] = names[name];
+    } else {
+      newnames[subname] = names[name];
+    }
+  });
+});
+
+names = newnames;
+newnames = {};
+
+Object.keys(names).forEach(name => {
+  let matches = name.split(/_+/g).filter(Boolean);
+
+  matches.forEach(subname => {
+    if (subname.length > 1 && !/^[^a-z]+$/gi.test(subname)) {
+      newnames[subname] = names[name];
+    }
+  });
+});
+
+names = newnames;
+newnames = {};
+
+fs.writeFileSync('prefix.txt', Object.keys(prefix).sort().join('\n'));
+fs.writeFileSync('dict.txt', Object.keys(dict).sort().join('\n'));
