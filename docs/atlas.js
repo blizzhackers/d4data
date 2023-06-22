d5beb5f5194976636ca2f608e7790b26fda1e9fb
BartonsGit <nak53188@gmail.com>
2023-06-22 17:21:00 -0400

    Optimization & Stylization
    Added identifier for markers (via id)
    Moved html rendering into data toggler to be piped into JS function
    
    Added new popups for markers
    Added new slide-out for markers

--- docs/atlas.js
+++ docs/atlas.js
@@ -2,7 +2,9 @@ function debounce(func, timeout = 480) {
   let timer;
   return (...args) => {
     clearTimeout(timer);
-    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
   };
 }
 
@@ -26,7 +28,7 @@ let doSearch = debounce(() => {
   }
 });
 
-function detectSearchUpdate () {
+function detectSearchUpdate() {
   if (prevSearchValue !== searchElement.value) {
     doSearch();
   }
@@ -36,3 +38,113 @@ function detectSearchUpdate () {
 }
 
 detectSearchUpdate();
+
+
+
+//Add clicking logic and style popups/sliders
+const pathElements = document.querySelectorAll('path[id="00000"]');
+const sliderContainer = document.querySelector('.slider-container');
+const pathDataElement = document.querySelector('.path-data');
+const popupBox = document.querySelector('.popup-box');
+
+pathElements.forEach((pathElement) => {
+  // Prevent default tooltip
+  pathElement.setAttribute('title', '');
+
+  // Add mouse event listeners
+  pathElement.addEventListener('mouseover', () => {
+    const popupContent = pathElement.getAttribute('data-popup-content');
+    const lines = popupContent.split('\n');
+    const formattedContent = lines.join('<br>');
+
+    popupBox.innerHTML = formattedContent;
+    popupBox.style.display = 'block';
+  });
+
+  pathElement.addEventListener('mouseout', () => {
+    popupBox.style.display = 'none';
+  });
+
+  pathElement.addEventListener('mousemove', (event) => {
+    const x = event.pageX;
+    const y = event.pageY;
+    popupBox.style.left = `${x}px`;
+    popupBox.style.top = `${y}px`;
+  });
+
+  pathElement.addEventListener('click', () => {
+    const popupContent = pathElement.getAttribute('data-popup-content');
+    const lines = popupContent.split('\n');
+  
+    // Dynamically generate the slider content based on the data
+    const sliderContent = document.querySelector('.slider-content');
+  
+    let xCoordinate = '';
+    let yCoordinate = '';
+  
+    sliderContent.innerHTML = `
+      <div class="close-btn">&times;</div>
+      ${lines
+        .map(line => {
+          const [label, value] = line.split(':');
+          const formattedLabel = label.trim();
+          const formattedValue = value.trim();
+  
+          if (formattedLabel === 'Coordinates') {
+            const [x, y] = formattedValue.split(',');
+            xCoordinate = x.trim();
+            yCoordinate = y.trim();
+            // Skip generating the "Coordinates" field
+            return '';
+          }
+  
+          return `
+            <div class="form-group">
+              <label>${formattedLabel}</label>
+              <input type="text" class="form-control" readonly value="${formattedValue}">
+            </div>
+          `;
+        })
+        .join('')}
+      <div class="form-group">
+        <label>X Coordinate</label>
+        <input type="text" class="form-control" readonly value="${xCoordinate}">
+      </div>
+      <div class="form-group">
+        <label>Y Coordinate</label>
+        <input type="text" class="form-control" readonly value="${yCoordinate}">
+      </div>
+      <div class="form-group center-text" style="padding-top: 20px;">
+        <button class="btn btn-primary btn-copy" style="">Copy Data</button>
+      </div>
+    `;
+  
+    document.querySelector('.close-btn').addEventListener('click', slideOutSlider);
+    slideInSlider();
+  
+    const btnCopy = document.querySelector('.btn-copy');
+    btnCopy.addEventListener('click', () => {
+      const formFields = sliderContent.querySelectorAll('input[type="text"]');
+      const data = Array.from(formFields)
+        .map(input => `${input.previousElementSibling.textContent}: ${input.value}`)
+        .join('\n');
+      navigator.clipboard.writeText(data).then(() => {
+        console.log('Data copied to clipboard:', data);
+        // You can show a success message or perform any other action here
+      }).catch(error => {
+        console.error('Failed to copy data to clipboard:', error);
+        // You can show an error message or handle the error as needed
+      });
+    });
+  });
+});
+
+// Slide in the slider so that it can slide while it slides
+function slideInSlider() {
+  sliderContainer.style.right = '100px';
+}
+
+// Slide out the slider so that it can slide while it slides
+function slideOutSlider() {
+  sliderContainer.style.right = '-500';
+}
