ffd38fc635c4d17816ccb7a81984c7084adfb023
Nishimura-Katsuo <38635886+Nishimura-Katsuo@users.noreply.github.com>
2023-11-20 09:10:51 -0700

    Added handling for windows in rebuild script.

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
+
+PATH="$(dirname $0)/CASCExplorer/CASCConsole/bin/Release/net6.0/:$PATH"
+CASCConsole="CASCConsole"
+
+# Add extension for windows if you're running in git bash
+if [[ "$OSTYPE" =~ ^msys ]]; then
+  CASCConsole="$CASCConsole.exe"
+fi
+
 rm data/* -rf
 curl https://d4armory.io/api/keys | node TactKey.js
-CASCConsole -o -m Pattern -e "base/*.dat" -d data/ -l enUS -p fenris
-CASCConsole -o -m Pattern -e "Base/meta/*" -d data/ -l enUS -p fenris
-CASCConsole -o -m Pattern -e "enUS_Text/meta/*" -d data/ -l enUS -p fenris
-CASCConsole -o -m Pattern -e "enUS_Speech/meta/*" -d data/ -l enUS -p fenris
+$CASCConsole -o -m Pattern -e "base/*.dat" -d data/ -l enUS -p fenris
+$CASCConsole -o -m Pattern -e "Base/meta/*" -d data/ -l enUS -p fenris
+$CASCConsole -o -m Pattern -e "enUS_Text/meta/*" -d data/ -l enUS -p fenris
+$CASCConsole -o -m Pattern -e "enUS_Speech/meta/*" -d data/ -l enUS -p fenris
 mv data/base/* data/Base/
 rm data/base/ -rf
 mv data/Base/ data/base/
