2b1eb896b0437a6039e2cf858e595d7d137d183e
Nishimura-Katsuo <38635886+Nishimura-Katsuo@users.noreply.github.com>
2024-02-21 20:05:56 -0700

    Added messages for rebuild script.

--- rebuild.sh
+++ rebuild.sh
@@ -9,21 +9,28 @@ if [[ "$OSTYPE" =~ ^msys ]]; then
   CASCConsole="$CASCConsole.exe"
 fi
 
+echo Removing old data.
 rm data/* -rf
+echo Fetching keys.
 curl https://d4armory.io/api/keys | node TactKey.js
+echo Fetching data.
 $CASCConsole -o -m Pattern -e "base/*.dat" -d data/ -l enUS -p fenris
 $CASCConsole -o -m Pattern -e "Base\\meta\\*" -d data/ -l enUS -p fenris
 $CASCConsole -o -m Pattern -e "enUS_Text\\meta\\*" -d data/ -l enUS -p fenris
 $CASCConsole -o -m Pattern -e "enUS_Speech\\meta\\*" -d data/ -l enUS -p fenris
 
 if ! [[ "$OSTYPE" =~ ^msys ]]; then
+  echo Fixing windows directory names.
   mv data/base/* data/Base/
   rm data/base/ -rf
   mv data/Base/ data/base/
 fi
 
 git checkout data/
+
+echo Removing old json.
 rm json/base/meta -rf
 rm json/enUS_Text/meta -rf
 rm json/enUS_Speech/meta -rf
+echo Parsing json.
 node parse.js data/
